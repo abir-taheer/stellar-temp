0906ca4b79b9705652ebc8d4168cb5606a61b057
Abir Taheer <abir@taheer.me>
2020-08-24 22:49:31 -0400

    fixed contact us component

--- comps/ContactUsCard.js
+++ comps/ContactUsCard.js
@@ -1,9 +1,26 @@
 import React from 'react';
+import Grid from '@material-ui/core/Grid';
+import layout from './../styles/Layout.module.css';
+import phone from './../img/phone.svg';
 
 export default function ContactUsCard() {
 	return (
 		<div>
-			<h3>Feel Free To Reach Out At Any Time</h3>
+			<h2 className={layout.title}>Feel Free To Reach Out</h2>
+			<Grid spacing={2} container justify={'center'}>
+				<Grid item xs={3}>
+					<img
+						className={layout.imageFit}
+						alt={'A phone'}
+						src={phone}
+					/>
+				</Grid>
+				<Grid item>
+					<p>646-703-3444</p>
+					<p>info@stellarcellardoors.com</p>
+					<p>Phone Quotes Gladly Provided!</p>
+				</Grid>
+			</Grid>
 		</div>
 	);
 }
--- pages/how-to-measure.js
+++ pages/how-to-measure.js
@@ -5,6 +5,25 @@ import BackButton from '../comps/BackButton';
 
 import diagram from './../img/door-measurements.png';
 import ContactUsCard from '../comps/ContactUsCard';
+import TableContainer from '@material-ui/core/TableContainer';
+import Paper from '@material-ui/core/Paper';
+import Table from '@material-ui/core/Table';
+import TableHead from '@material-ui/core/TableHead';
+import TableRow from '@material-ui/core/TableRow';
+import TableCell from '@material-ui/core/TableCell';
+import TableBody from '@material-ui/core/TableBody';
+
+const gordonDoors = [
+	{ model: 'CD-3', length: 71, width: 53, height: 19.5 },
+	{ model: 'CD-3', length: 72, width: 55, height: 19.5 },
+	{ model: 'CD-2', length: 63, width: 49, height: 22 },
+	{ model: 'CD-2', length: 64, width: 51, height: 22 },
+	{ model: 'CD-SL', length: 42.5, width: 49, height: 51 },
+	{ model: 'CD-SL', length: 43.25, width: 51, height: 52 },
+	{ model: 'CD-0', length: 54, width: 45, height: 30 },
+	{ model: 'CD-0', length: 55, height: 47, width: 30 },
+	{ model: 'CD-1', length: 57, width: 45, height: 24.5 },
+];
 
 const HowToMeasure = () => {
 	return (
@@ -39,11 +58,53 @@ const HowToMeasure = () => {
 						</li>
 					</ol>
 					<img
-						className={layout.imageFit}
+						className={layout.imageFit + ' ' + layout.bottomMargin}
 						style={{ width: '400px' }}
 						src={diagram}
 						alt={'Diagram of how to measure a door'}
 					/>
+
+					<h2>Stock Gordon Doors</h2>
+
+					<TableContainer
+						component={Paper}
+						className={layout.bottomMargin}
+					>
+						<Table aria-label="simple table">
+							<TableHead>
+								<TableRow>
+									<TableCell>Model</TableCell>
+									<TableCell align="right">
+										Length (in)
+									</TableCell>
+									<TableCell align="right">
+										Width (in)
+									</TableCell>
+									<TableCell align="right">
+										Height (in)
+									</TableCell>
+								</TableRow>
+							</TableHead>
+							<TableBody>
+								{gordonDoors.map((row, index) => (
+									<TableRow key={index}>
+										<TableCell component="th" scope="row">
+											{row.model}
+										</TableCell>
+										<TableCell align="right">
+											{row.length}"
+										</TableCell>
+										<TableCell align="right">
+											{row.width}"
+										</TableCell>
+										<TableCell align="right">
+											{row.height}"
+										</TableCell>
+									</TableRow>
+								))}
+							</TableBody>
+						</Table>
+					</TableContainer>
 					<ContactUsCard />
 				</main>
 			</div>

07110353192f97526112b170dccfe0da97c6d6f9
Abir Taheer <abir@taheer.me>
2020-08-24 23:03:08 -0400

    added phone to gallery page

--- comps/GalleryPageTemplate.js
+++ comps/GalleryPageTemplate.js
@@ -3,6 +3,7 @@ import layout from '../styles/Layout.module.css';
 import BackButton from './BackButton';
 import Gallery from './Gallery';
 import React from 'react';
+import ContactUsCard from './ContactUsCard';
 
 const GalleryPageTemplate = ({ images, title, tagline }) => {
 	return (
@@ -15,6 +16,8 @@ const GalleryPageTemplate = ({ images, title, tagline }) => {
 					<p>{tagline}</p>
 					<p>Click on any of the photos to view them up close</p>
 					<Gallery images={images} />
+					<br />
+					<ContactUsCard />
 				</main>
 			</div>
 		</>
